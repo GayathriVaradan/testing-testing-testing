9bcccecad61a2278b2eb1bc58ae8a94a15d162b2
Dimitrios Fasoulas <dimitrios.fasoulas@comeon.com>
2020-09-18 10:11:13 +0300

    Simple counter app with tests

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    let shouldCleanUp = false;
+    async function load() {
+      const res = await fetch('/getcounter');
+      const data = await res.json();
+      if (!shouldCleanUp) {
+        setCounter(data.counter);
+      }
+    }
+    load();
+    // Cleaning up the effect. This will be called when the component unmounts
+    // In our case this will stop errors happening from tests that unmount the
+    // after they are done but before the call to the api is completed. In that
+    // case the setCounter would fire for the unmounted component causing the warrning
+    return () => (shouldCleanUp = true);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1 data-testid='title'>React counter app demo</h1>
       </header>
+      <main>
+        <div>
+          <button
+            onClick={() => {
+              setCounter(counter - 1);
+            }}
+            data-testid='dec-button'
+          >
+            -
+          </button>
+          <span data-testid='counter' className='counter'>{counter}</span>
+          <button
+            onClick={() => {
+              setCounter(counter + 1);
+            }}
+            data-testid='inc-button'
+          >
+            +
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,70 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('Counter app testing', ()=> {
+
+
+  const server = setupServer(
+    rest.get('/getcounter', (req, res, ctx)=> {
+      return res(ctx.json({counter: 33}));
+    })
+  );
+
+  beforeAll(()=> { server.listen()});
+  let getByTestId;
+  let findByText;
+  beforeEach(async ()=> {
+    ({ getByTestId, findByText } = render(<App/>));
+  })
+  afterEach(()=> { 
+    server.resetHandlers()
+  });
+  afterAll(()=> { server.close()});
+
+  test('renders app title', ()=> {
+    const title = getByTestId('title'); 
+    expect(title.innerHTML).toBe('React counter app demo');
+  });
+
+  test('renders an increment btn', ()=> {
+    const incBtn = getByTestId('inc-button');
+    expect(incBtn).toHaveTextContent('+');
+  });
+
+  test('renders an decrement btn', ()=> {
+    const decBtn = getByTestId('dec-button');
+    expect(decBtn).toHaveTextContent('-');
+  });
+
+  test('renders a counter', ()=> {
+    const counter = getByTestId('counter');
+    expect(counter).toHaveTextContent('0');
+  });
+  
+  test('Clicking the + button should increase the counter', ()=> {
+    const counter = getByTestId('counter');
+    const incBtn = getByTestId('inc-button');
+    userEvent.click(incBtn);
+    expect(counter).toHaveTextContent('1');
+  });
+  
+  test('Clicking the - button should decrease the counter', ()=> {
+    const counter = getByTestId('counter');
+    const decBtn = getByTestId('dec-button');
+    userEvent.click(decBtn);
+    expect(counter).toHaveTextContent('-1');
+  });
+  
+  /**
+   * Async test so we wait for the api call
+   */
+  test('Initial counter state should be fetched by an external API', async ()=> {
+    expect(await findByText('33')).toBeInTheDocument();
+  });
+
+})
